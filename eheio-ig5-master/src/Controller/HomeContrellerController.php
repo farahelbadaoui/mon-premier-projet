fffb28f74cd4f689005242d582cfe8d2827aafec
Farahel Badaoui <Farah.bd.99@gmail.com>
2024-11-16 10:36:11 +0000

    twig

---
+++ eheio-ig5-master/src/Controller/HomeContrellerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeContrellerController extends AbstractController
+{
+    #[Route('/home/contreller', name: 'app_home_contreller')]
+    public function index(): Response
+    {
+       return new Response(content:"Hello World !");
+    }
+    #[Route(path:"/about", name:"app_about")]
+    public function about():Response
+    {
+        return new Response( content:"hello World 2!");
+    }
+    #[Route("/home/contact",name:"app_home_contact")]
+    public function contact():Response
+    {
+        return new Response(content:"WeLcome to contact");
+    }
+}
